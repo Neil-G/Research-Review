a9cea7af8c9031641a5234580b288f772cc8db54
neil <ngneilgordon@gmail.com>
2016-04-10 18:11:25 -0400

    move Entry and Form into separate files

---
+++ src/components/Entry.react.js.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+export class Entry extends Component {
+	render(){
+		const { entry } = this.props
+		return(
+			<div 
+        key={entry.createdAt} 
+        style={{ border: '3px solid gray', padding: '12px', position: 'relative', maxWidth: '600px', maxHeight: '600px', overflowY: 'scroll' }}>
+        <h1 style={{ float: 'left', display: 'inline-block', marginBottom: '0'}}>
+          { entry.source } <span style={{ fontSize: '0.3em'}} > { entry.type } </span>
+        </h1>
+
+        <div style={{ float: 'right' }} >
+          Edit
+        </div>
+
+
+        <h4 style={{ clear: 'both' }}>{ entry.title }</h4>
+        <div>{ entry.description }</div>
+
+  <div hidden={false} style={{ maxHeight: 1000, overflow: 'hidden', transition: 'all 0.8s'}} >    
+      {/* Terms */}
+        <div>
+          <b>Terms</b> <br/> 
+          <ul>
+          { 
+            entry.terms.map( term => <li>{term.name}<ul><li>{term.definition}</li></ul></li>) 
+          }
+          </ul>
+        </div>
+        <div>
+          <ul>
+          
+      {/* Points */}
+          <b>Points</b>
+          { entry.points.map( point => {
+              return <li> {point}. </li>
+            }) 
+          }
+          </ul>
+        </div>
+  </div>      
+      {/* Tags */}
+        <div>
+          { entry.tags.map( tag => {
+              return( 
+                <span style={{ border: '1px solid gray', borderRadius: '100px', padding: '6px' }}> {tag} </span>
+              )
+            }) 
+          }
+        </div>
+      </div>
+		);
+	}
+}
---
+++ src/components/EntryCreateOrUpdateForm.react.js
@@ -0,0 +1,228 @@
+import React, { Component } from 'react';
+
+export class EntryCreateOrUpdateForm extends Component {
+	constructor(props){
+    super(props)
+    this.state = { 
+        terms: []
+      , points: []
+      , tags: []
+    }
+    this.addPoint = this.addPoint.bind(this);
+    this.addTag = this.addTag.bind(this);
+
+    this.addTerm = this.addTerm.bind(this);
+    this.updateTerm = this.updateTerm.bind(this)
+    this.deleteTerm = this.deleteTerm.bind(this)
+
+    this.createEntry = this.createEntry.bind(this)
+
+    this.updatePointorTag = this.updatePointorTag.bind(this)
+    this.deletePointorTag = this.deletePointorTag.bind(this)
+  }
+
+  updateTerm(e, key, index){
+    e.preventDefault();
+    this.state.terms[index][key] = e.target.value;
+    this.setState({ terms: this.state.terms })
+  }
+
+  updatePointorTag(e, arrayName, index){
+    e.preventDefault();
+    this.state[arrayName][index] = e.target.value;
+    this.setState({ tags: this.state.tags, points: this.state.points })
+  }
+
+  deletePointorTag(arrayName, index){
+    this.state[arrayName].splice(index, 1)
+    this.setState({ tags: this.state.tags, points: this.state.points })
+  }
+
+  addTerm(e){
+    e.preventDefault();
+    this.state.terms.unshift({ name: '', definition: ''});
+    this.setState({ terms: this.state.terms });
+  }
+
+  deleteTerm(e, index){
+    e.preventDefault();
+    this.state.terms.splice(index, 1)
+    this.setState({ terms: this.state.terms })
+  }
+
+  addPoint(e){
+    e.preventDefault();
+    this.state.points.unshift('');
+    this.setState({ points: this.state.points });
+  }
+
+  addTag(e){
+    e.preventDefault();
+    this.state.tags.unshift(this.refs["add-tag-input"].value.trim());
+    this.setState({ tags: this.state.tags });
+    this.refs["add-tag-input"].value = "";
+  }
+
+  createEntry(e){
+    e.preventDefault();
+    const newEntry = {
+        createdAt: Date.now()
+      , source: this.refs['source-input'].value.trim()
+      , title: this.refs['title-input'].value.trim()
+      , description: this.refs['description-input'].value.trim()
+      , type: this.refs['type-input'].value.trim()
+      , terms: this.state.terms
+      , points: this.state.points
+      , tags: this.state.tags
+    }
+    console.log("newEntry", newEntry)
+    this.props.onCreateEntryClick(newEntry)
+  }
+
+	render(){
+		const { terms, points, tags } = this.state
+		const { formHidden } = this.props
+		return(
+		<form 
+          style={{ 
+            border: '1px solid gray'
+            , boxSizing: 'border-box'
+            , padding: '12px'
+            , background: 'white'
+            , maxWidth: '600px'
+            , width: '96%'
+            , position: 'fixed'
+            , top: '80px'
+            , bottom: '4px'
+            , left: '8px'
+            , overflowY: 'scroll'
+            , transition: 'all 1s'
+            , transform: formHidden ? 'translateX(-700px)' : 'translateX(0px)'
+          }}>
+          
+          <h2 style={{ display: 'inline-block', float: 'left'}} > Create New Entry </h2> 
+          <button style={{ float: 'right'}} > close </button>
+
+        {/* Source Input */}
+          <div style={{ border: '1px solid gray', width: '100%', position: 'relative', height: '80px', clear: "both" }}>
+            <label style={{ position: 'absolute', left: '10px' }} >Source</label>
+            <input 
+              type="text" 
+              ref="source-input"
+              style={{ width: '100%', height: '100%', borderRadius: '0', marginBottom: '0' }} />
+          </div>
+
+        {/* Title Input */}
+          <div style={{ border: '1px solid gray', width: '100%', position: 'relative', height: '80px', borderTop: '0' }}>
+            <label style={{ position: 'absolute', left: '10px' }} >Title</label>
+            <input 
+              type="text"
+              ref="title-input" 
+              style={{ width: '100%', height: '100%', borderRadius: '0', marginBottom: '0' }} />
+          </div>
+
+{/* Description Input */}
+          <div style={{ border: '1px solid gray', width: '100%', position: 'relative', height: '80px', borderTop: '0' }}>
+            <label style={{ position: 'absolute', left: '10px' }} >Description</label>
+            <input
+              ref="description-input" 
+              type="text" 
+              style={{ width: '100%', height: '100%', borderRadius: '0', marginBottom: '0' }} />
+          </div>
+
+{/* Type Input */}
+          <div style={{ border: '1px solid gray', width: '100%', position: 'relative', height: '80px', borderTop: '0' }}>
+            <label style={{ position: 'absolute', left: '10px' }}>Type</label>
+            <input 
+              ref="type-input"
+              type="text" 
+              style={{ width: '100%', height: '100%', borderRadius: '0', marginBottom: '0' }} />
+          </div>
+    
+          
+
+{/* Terms Input */}
+          <label style={{ display: "inline-block"}}> Terms </label>  
+          <span onClick={this.addTerm}> + </span>
+          {
+            terms.map( (term, index) => {
+              return(
+                <div style={{ border: '1px solid gray', height: '100px', position: 'relative', overflow: 'hidden', marginBottom: '8px'}}> 
+
+                  <input 
+                    type="text" 
+                    placeholder="name" 
+                    value={terms[index].name}
+                    onChange={e => this.updateTerm(e, 'name', index)} 
+                    style={{ width: '100%', borderRadius: '0', position: "absolute", height: '40px' }} 
+                  />
+                  <span 
+                    style={{ position: "absolute", top: "4px", right: "8px"}}
+                    onClick={e => this.deleteTerm(e, index)}
+                  > 
+                    X 
+                  </span> 
+                  <input 
+                    type="text" 
+                    placeholder="definition" 
+                    value={terms[index].definition} 
+                    onChange={e => this.updateTerm(e, 'definition', index)} 
+                    style={{ width: '100%', position: "absolute", top: '40px', height: '60px', borderRadius: '0' }} 
+                  />
+                </div>
+              );
+            })
+          }
+
+          <br/>
+          <label style={{ display: "inline-block"}}> Points </label>
+          <span onClick={this.addPoint}> + </span>
+          <ul>
+          
+          {
+            points.map( (point, index) => {
+              return(
+              <div style={{ position: 'relative'}} > 
+{/* Points Input */}
+                <textarea 
+                  style={{ width: "100%", position: "relative", height: '100%', maxWidth: "100%" }}
+                  onChange={e => this.updatePointorTag(e, 'points', index) }
+                  value={point}
+                />
+                <span 
+                  onClick={ () => this.deletePointorTag('points', index) }
+                  style={{ position: 'absolute', top: '0px', right: '8px'}}
+                > 
+                X 
+                </span>
+              </div> 
+              )
+
+            })
+          }
+          </ul>
+
+
+{/* Tags Input */}
+          <label> Tags </label>
+          <input type="text" ref="add-tag-input"/> 
+          <span onClick={this.addTag}> + </span>
+          <div style={{ marginBottom: '12px'}}  >
+          {
+            tags.map( (tag, index) => {
+              return( 
+                <span 
+                  key={tag}
+                  onClick={() => this.deletePointorTag('tags', index)} 
+                  style={{ padding: '6px 18px', border: "1px solid gray", borderRadius: '100', marginRight: '4px', marginBottom: '4px'}}> 
+                  {tag} 
+                </span>);
+            })
+          }
+          </div>
+          <button style={{ width: "100%" }} onClick={ this.createEntry }> create new entry </button>
+          
+        </form>
+		)
+	}
+}
